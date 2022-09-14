c01f9f373be3a1225529c0e8e47cf6c530b105f9
jt0dd <jt0dd1995@gmail.com>
2022-09-14 16:43:56 -0400

    updated text

--- index.js
+++ index.js
@@ -124,12 +124,12 @@ const blogBody = new Element('div', {
 
     self.append((new Element("div", {
       styles: {
-        'padding': '0px 15px'
+        'padding': '20px 15px'
       },
       ready: self => {
         self.setHTML(marked.parse(`
 ## Chapter 1 | Bottom Line Up Front (BLUF)
-*Communication is half of the battle.*
+*Communication is half of every battle.*
 
 The first lesson of cybersecurity comes to us courtesy of the U.S. Army; a trick I learned in my time enlisted working at the National Security Agency: Begin every professional writing with a BLUF.
 
@@ -143,7 +143,7 @@ In this saga, I've *already* introduced the most important skills of the profess
 
 ## Chapter 2 | Engineering *Outcome*, *Outcome*, *Outcome*
 
-The reason we first pause the engineering aspect of this "Security Engineering" Saga to highlight these "soft-skill" aspects of the profession is simple: All the technical expertise and skill in the world will lead you nowhere if you don't first understand what you're really supposed to be *doing* with it. ***Outcome.*** If your only focus in your job is cybersecurity, *you will fail at cybersecurity.* From now on, you are a **multi-discipline** professional. Your new discipline? Enabling operational outcome. That's your job. Before analyzing network protocols, forensic endpoint analysis, reverse engineering, DevSecOps, you need to understand that the very core of your purpose, from the perspective of your employer is either:
+The reason we first pause the engineering aspect of this "Security Engineering" Saga to highlight these "soft-skill" aspects of the profession is simple: All the technical expertise and skill in the world will lead you nowhere if you don't first understand what you're really supposed to be *doing* with it. ***Outcome.*** If your only focus in your job is cybersecurity, *you will fail at cybersecurity.* From now on, you are a **multi-discipline** professional. Your new discipline? Enabling operational outcome. That's your job. Before analyzing network protocols, forensic endpoint analysis, reverse engineering, DevSecOps, you need to understand that the very core of your purpose (from the perspective of your employer) is either:
 
 - Meet some perceived regulatory compliance requirements (security theater),
 - (or) Enable the business to continue to operate its organizational objectives in order of priority under limited resource constraints.
@@ -169,35 +169,38 @@ The content of this piece (and I suspect all communication for the rest of my li
 
 > The. Most. Important. Thing.
 
-> If you're sorta thinking, 'huh? You mean like, blind and deaf people Accessibility?' then you're not alone, because I've come to understand that there are lots and LOTS of people just like you: people for whom this idea does not have the right Accessibility, so it hasn't been able to get through to you yet. It's not your fault for not understanding, any more than it would be your fault for being blind or deaf or motion-restricted or living with any other disability. When software -- or idea-ware for that matter -- fails to be accessible to anyone for any reason, it is the fault of the software or of the messaging of the idea. It is an Accessibility failure. [...] But I'll argue that Accessibility is actually more important than Security because dialing Accessibility to zero means you have no product at all, whereas dialing Security to zero can still get you a reasonably successful product such as the Playstation Network." - [Steve Yegge](https://www.linkedin.com/in/steveyegge/), Senior Software Engineer & Manager, F**A**AN**G**, [Stevey's Google Platforms Rant](https://gist.github.com/chitchcock/1281611)
+> If you're sorta thinking, 'huh? You mean like, blind and deaf people Accessibility?' then you're not alone, because I've come to understand that there are lots and LOTS of people just like you: people for whom this idea does not have the right Accessibility, so it hasn't been able to get through to you yet. It's not your fault for not understanding, any more than it would be your fault for being blind or deaf or motion-restricted or living with any other disability. When software -- or idea-ware for that matter -- fails to be accessible to anyone for any reason, it is the fault of the software or of the messaging of the idea. It is an Accessibility failure. [...] But I'll argue that Accessibility is actually more important than Security because dialing Accessibility to zero means you have no product at all, whereas dialing Security to zero can still get you a reasonably successful product such as the Playstation Network." - [Steve Yegge](https://www.linkedin.com/in/steveyegge/), Senior Software Engineer & Manager, [Stevey's Google Platforms Rant](https://gist.github.com/chitchcock/1281611)
 
 For those who need it, we begin this saga with a substantial overview priceless mile-high lessons on how to approach security engineering (and more broadly, cybersecurity as a profession). If you prefer, feel free to skip straight ahead to the experiments and hand-on coding sections, depending on your interests:
 
-- **Chapter 10 | Memory Forensics:** Forensic analysis (first manual, then automated) of a vulnerable peer-to-peer web app (Chess).
-- **Chapter 11 | Remote Code Execution:** Engineering an attack to exploit vulnerable code and achieve Remote Code Execution (RCE).
-- **Chapter 12 | Detection:** Building a run-time memory forensics baseline of the JavaScript runtime within Google Chrome.
+- **Chapter 7 | Let's Exploit Chess (Basic JavaScript Analysis):** Discovering vulnerability in a simple web-app designed without security in mind (a common scenario).
+- **Chapter 8 | Memory Forensics:** Forensic analysis (first manual, then automated) of a vulnerable peer-to-peer web app (Chess).
+- **Chapter 9 | Remote Code Execution:** Engineering an attack to exploit vulnerable code and achieve Remote Code Execution (RCE).
+- **Chapter 10 | Detection:** Building a memory forensics baseline of the JavaScript run-time environment within Google Chrome.
 
 What I just did was take the lesson about operational accessibility and apply it, as I propose we all should, to communication (and everything else we do that involves other people). Even though a writer must establish a target audience, each member of that audience will still consume the writing from a remarkably different perspective. A different set of experiences, strengths, goals, and objectives. Introducing and facilitating choices enabling consumers of your content to quickly access sections that provide them the greatest value (and skip the rest) is an awesome way to apply that lesson to communication.
 
-Popular wisdom: Use a hook in your communication content to highlight the most interesting feature (value).
+Popular wisdom: Use hooks in your communication content to highlight the most interesting feature (value). So here's an example:
 
 This Saga culminates, after journeying through the many fundamental challenges of security (through the initially simple abstraction of a Chess game) in two practical outcomes far more remarkable than a shallow one-off exercise:
 
 - Checkmate: A real, operational C2 framework operating within the memory run-time of Google Chrome, featuring some of the most effective detection counter-measures known to modern offensive cyber operations.
-- Rematch: A real, comprehensive Detection-as-Code solution to **S**ecure, **O**rachestrate, **A**utomate, and **R**espond (SOAR) effectively against this state-of-the-art attack.
+- Rematch: A real, comprehensive Detection-as-Code solution to Secure, Orachestrate, Automate, and Respond (SOAR) effectively against this state-of-the-art attack.
 
 ## Chapter 4 | Engineering Limitless Value
 *The highest value adding role in the cybersecurity field.*
 
-If you're still reading, the BLUF worked. Time for a hook: By the end of this saga, you're going to reimagine, thanks to Chess, the possibilities of engineering powerful cybersecurity solutions at scale. But don't just take it from me, every chapter I promise you a quote from someone smarter than I - for a total of 74 legendary bits of wisdom from great leaders and reknowned heroes, your Gandalfs and Dumbledores of the cyber world, as any good saga must have.
+If you're still reading, the BLUF and hook worked. Let's try one more hook: By the end of this saga, you're going to reimagine, thanks to Chess, the possibilities of engineering powerful cybersecurity solutions at scale. But don't just take it from me, every chapter I promise you a quote from someone smarter than I - for a total of 74 legendary bits of wisdom from great leaders and reknowned heroes, your Gandalfs and Dumbledores of the cyber world, as any good saga must have.
 
 If you managage risk on a network in some way: identify vulnerability, apply controls, detect a threat, or perhaps create one, you're adding value to your cyber mission, once. If you engineer a tool for it, you're adding value to every cyber mission, many times over. Your time and effort **return on investment** (RoI) has *limitless* potential.
 
-> Quote about adding and communicating value
+> "In short, I conceive that great part of the miseries of mankind are brought upon them by the false estimates they have made of the value of things, and by their giving too much for their whistles." - Benjamin Franklin, 1779
 
-Remember: Most leaders think in terms of value and cost. Always bring real value to the table, and communicate concisely why the return on investment to realize that value is both higher than the alternatives and worthwhile. We're not in the business of security for security's sake. We're in the business of profit. *By adding limitless value potential (and effectively communicating it), a Security Engineer thrives in the business of profit.*
+When Ben was about seven years old, he bought a whistle for far more than it was worth. He later wrote a letter citing that lesson about how important that lesson had been throughout, to properly assess value against his time, effort, and attention.
 
-But remember: Limitless value potential never becomes tangible without execution. Don't spend all your time dreaming, lest you forget to transform your ideas into working code and tangible outcomes.
+Remember: Most leaders think in terms of risk, cost, and benefit (value). If you want to obtain buy-in for your work and your ideas, always bring real value to the table. Communicate concisely why the return on investment to realize that value is both higher than the alternatives and worthwhile. We're not in the business of security for security's sake. We're in the business of profit. *By adding limitless value potential (and effectively communicating it), a Security Engineer thrives in the business of profit.*
+
+Limitless value *potential* never becomes tangible without execution. Don't spend all your time dreaming, lest you forget to transform your ideas into working code and tangible outcomes.
 
 ## Chapter 5 | You, Too, Can Be a Security Engineer
 *Security engineering is the most powerful secondary discipline you can pick up in any cyber role.*
@@ -219,7 +222,19 @@ No matter your role...
 
 ...you probably do or observe some task in your department's daily workflow which could be automated and could save the company time (money) across one or more departments of labor. If you hate the idea of writing code, read no further. But if you already code or are open to learn, this saga is for you.
 
-> Quote about automation
+But this isn't just a saga about automating a few SIEM queries. This is about embracing a new era of tooling driving a revolution in the way cybersecurity risk is managed at scale. This is about meeting a dire and growing need in the industry.
+
+> "I think looking at the cognizant science in this field is really important because the work is really intense if you're doing it all the time. And that's another reason why people fall out of it [...] At Google, I don't know anyone who's been in my team who hasn't asked for a job change after 4 years. [...] The work gets repetitive, it gets boring, the assistants are not helpful enough. And this is a problem. If you're a manager and you're building your SOC, you've gotta know that your people aren't gonna stay for very long." - Heather Adkins, Director of Information Security, Google, 2017
+
+Security engineering can and must be the cornerstone of solving problems like this one. Heather went on to make a prediction:
+
+> "But let's look at the future, because I think that the future holds something quite valuable, and I'm going to be somewhat controversial with this. I think, ultimately, by 2050 or so, we will have put in place something to solve James Anderson's original problem. We will have systems that can defend themselves. And they'll be better constructed or they'll be self-healing."
+
+And I believe Heather's prediction. I'll go a step further and predict that we'll be pretty close to this solution by 2030, if not sooner. To those in the audience who scoff at this idea, citing the extremely weak state of cyber security today and the apparent lack of interest in most organizations to address it: I counter that attack automation technology will radpidly evolve and with it, the frequency and severity of attack, leading to a rise in losses and therefore a rise in business interest in better defense. And I counter that this kind of automation, as has been the case with all industrial automation ventures throughout history, will cut costs and increase profit margins, further compounding the pressure for organizations to pursue it.
+
+The only question is: Will you be leading the way to build that new era of value? Or will you sit on the sidelines as your job is automated away?
+
+Before we move on, let's cover a few of the major ways automation can produce value for an organization, especially when it comes to cybersecurity where "the work is really intense" and the labor cost is extremely high.
 
 #### Integration
 
@@ -242,10 +257,15 @@ And if you're smart, you'll end up being compensated very well for that skillset
 ## Chapter 6 | What This Saga Is Not
 *This is not a cybersecurity & software engineering "noob-to-pro" tutorial.*
 
-- Coding (in Python) will be demonstrated, but not elaborated on comprehensively.
+- Coding (in Python) will be demonstrated and briefly explained, but not elaborated on comprehensively.
 - Basic cybersecurity concepts will be referenced, but not explained. Advanced concepts will be more thoroughly explained.
 
 In other words, the reader is expected to know the basics of both subjects. Someone who has passed Sec+ and completed a few small Python projects with 6-12 months of experience coding should be fairly well prepared for this learning saga.
+
+> "Be more concerned with not learning than not knowing. Far too often due to feelings of inadequacy, imposter syndrome, ego and so on, people will refrain from asking questions in meetings and on projects because they will get exposed as not knowing things. I prefer to flip this paradigm on its head and I'm far more concerned with not learning what I don't know, than keeping up the facade that I do know. People are eager to share their experience, insights and expertise, you just have to be willing to ask. The irony of this is, when you shed that defense tactic, you learn a hell of a lot more and are more competent in the long haul as well. If we don't learn, we don't grow, and if we don't grow, we lose." - Chris Hughes, CISO & Co-Founder, Aquia, 2022
+
+## Chapter 7 | Let's Exploit Chess (Basic JavaScript Analysis)
+*The JavaScript behind this Chess engine will not be an area of focus beyond very basic reverse engineering. If anyone wants to see the full, completed (and vulnerable!) Chess engine source code, it's on GitHub.*
 
 `))
       }
